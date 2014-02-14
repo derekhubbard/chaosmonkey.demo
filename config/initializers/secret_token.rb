1fc57265840eff74a0b4f37f2da82b75274e9b90
Derek Hubbard <derek.hubbard@pomiet.com>
2014-02-14 11:49:17 -0500

    Initial checkin.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChaosmonkeyDemo::Application.config.secret_key_base = 'a5220426cc5281493eb9264a6f2de8b87874ad465373730b8be81a886fc3550b1d71d2e85eeb616f349abf40d1e798db3c7e2c458d39e712886d3711699c2657'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChaosmonkeyDemo::Application.config.session_store :cookie_store, key: '_chaosmonkey_demo_session'
